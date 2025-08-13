20cf1e553f4def13091637739022dc18bc4e4261
Grealishgit <eugyneehunter@gmail.com>
2025-08-13 11:47:52 +0300

    Added a hero section

--- app/Views/hero.php
+++ app/Views/hero.php
@@ -1,4 +1,30 @@
-<div>
-    <h1 class="text-4xl font-bold text-center my-8">Welcome to  <span class="text-blue-500">Blue</span>Life Hospital</h1>
-    <p class="text-lg text-center">Your health is our priority. We provide the best medical services.</p>
+<div class="flex flex-col bg-gray-100 items-center p-10 mt-10 w-full min-h-screen"
+>
+<!-- style="background-image: url('/storage/uploads/bg1.jpg'); background-size: cover; background-position: center;" -->
+
+ <div class=" rounded-md flex  mt-5 bg-blue-500 w-full">
+    <!-- Heading to the left -->
+    <div class="flex-1 items-center justify-center flex flex-col">
+        <h1 class="text-6xl font-bold text-center text-white  my-8">
+        Welcome to BlueLife Hospital
+    </h1>
+    <p class="text-lg text-white">
+        Your health is our priority. We provide the best medical services.
+    </p> 
+    <div class="mt-6 flex space-x-4">
+        <a href="#" class="bg-white text-blue-500 px-6 py-3 rounded-lg shadow-md hover:bg-gray-100 transition duration-300">
+            Get Started
+        </a>
+         <a href="#" class="bg-white text-blue-500 px-6 py-3 rounded-lg shadow-md hover:bg-gray-100 transition duration-300">
+            Book Appointment
+        </a>
+    </div>
+    </div>
+    
+    <!-- Image to the right -->
+    <div class="flex-1 flex flex-col">
+        <img src="/storage/uploads/header.png" alt="BlueLife Hospital" class="w-full max-w-xl" />
+    </div>
+ </div>
+   
 </div>
--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,5 +1,5 @@
 
-<nav class="bg-white text-white p-4 shadow-md shadow-gray-200 flex items-center justify-between">
+<nav class="bg-white fixed top-0 w-full text-white p-4 shadow-sm shadow-gray-600 flex items-center justify-between">
     <div class="flex items-center space-x-2">
         <span class="font-bold cursor-pointer  text-blue-500 text-2xl">Blue<span class="text-black">Life</span> </span>
     </div>
