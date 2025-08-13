af735241a92a65756fa854f881b2f621c2f05c7e
Grealishgit <eugyneehunter@gmail.com>
2025-08-13 15:59:06 +0300

    Add doctors section to the main layout

---
+++ app/Views/doctors.php
@@ -0,0 +1,6 @@
+<div class='flex flex-col mt-10 items-center'>
+    <p class="text-2xl font-semibold">Top Doctors to Book</p>
+    <p class="text-center text-gray-800">
+        Simply browse through our extensive list of trusted doctors.
+    </p>
+</div>
