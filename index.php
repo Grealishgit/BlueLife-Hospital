db87e03b13c5231181aefa10e4d278a033ff8063
Grealishgit <eugyneehunter@gmail.com>
2025-08-14 00:23:49 +0300

    Move CSS styles to the end of the body for better structure and loading performance

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
     <?php include 'app/Views/doctors.php'; ?>
     <?php include 'app/Views/footer.php'; ?>
 </body>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Signika:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+</style>
 
+<style>
+    body {
+        font-family: 'Quicksand', sans-serif;
+    }
+</style>
 
 </html>
