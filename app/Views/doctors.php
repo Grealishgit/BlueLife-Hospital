249a77bde6b00905b962b9437b390ed770465218
Grealishgit <eugyneehunter@gmail.com>
2025-08-15 22:36:06 +0300

    Update doctor section title styling for improved visibility

--- app/Views/doctors.php
+++ app/Views/doctors.php
@@ -1,6 +1,6 @@
 <div class='flex flex-col mt-10 items-center'>
-    <p class="text-2xl font-semibold">Top Doctors to Book</p>
-    <p class="text-center text-gray-800">
+    <p class="text-3xl font-bold">Top Doctors to Book</p>
+    <p class="text-center font-semibold text-gray-800">
         Simply browse through our extensive list of trusted doctors.
     </p>
 </div>
