2ac2c571424326f16624894ae9ad342feed3b76f
Grealishgit <eugyneehunter@gmail.com>
2025-08-15 22:37:24 +0300

    Update doctor section title for improved emphasis on "Doctors"

--- app/Views/doctors.php
+++ app/Views/doctors.php
@@ -1,5 +1,5 @@
 <div class='flex flex-col mt-10 items-center'>
-    <p class="text-3xl font-bold">Top Doctors to Book</p>
+    <p class="text-3xl font-bold">Top <span class="text-blue-500">Doctors</span> to Book</p>
     <p class="text-center font-semibold text-gray-800">
         Simply browse through our extensive list of trusted doctors.
     </p>
