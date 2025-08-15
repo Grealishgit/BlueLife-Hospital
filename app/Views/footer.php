b3de4e1cefde1264e3bb47bf82ea10ff4a41d705
Grealishgit <eugyneehunter@gmail.com>
2025-08-15 22:34:25 +0300

    Update footer layout for improved responsiveness and accessibility

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,10 +1,11 @@
 <footer class="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
     <div class="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
         <div class="md:max-w-96">
-            <img class="h-9"
+            <!-- <img class="h-9"
                 src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoDark.svg"
-                alt="dummyLogoDark">
-            <p class="mt-6 text-sm">
+                alt="dummyLogoDark"> -->
+            <h2 class="font-bold text-black text-2xl">Blue<span class="text-blue-500">Life</span></h2>
+            <p class="mt-2 text-sm">
                 Bluelife is a leading healthcare provider, committed to delivering high-quality medical services and
                 patient care.
                 We prioritize your health and well-being to ensure you receive the best possible care.
@@ -24,12 +25,12 @@
                 <h2 class="font-semibold text-gray-800 mb-5">Subscribe to our newsletter</h2>
                 <div class="text-sm space-y-2">
                     <p>The latest news, articles, and resources, sent to your inbox weekly.</p>
-                    <div class="flex items-center gap-2 pt-4">
+                    <div class="flex md:flex-row flex-col items-center gap-2 pt-4">
                         <input
                             class="border border-gray-500/30 placeholder-gray-500 focus:ring-2 ring-blue-600 outline-none w-full max-w-64 h-9 rounded px-2"
                             type="email" placeholder="Enter your email">
                         <button
-                            class="bg-blue-500 hover:text-white hover:text-blue-600 cursor-pointer w-24 h-9 text-white rounded">Subscribe</button>
+                            class="bg-blue-500 hover:text-white hover:text-blue-600 cursor-pointer md:w-24 w-full h-9 text-white rounded">Subscribe</button>
                     </div>
                 </div>
             </div>
