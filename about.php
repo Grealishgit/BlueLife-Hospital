3ffdddd85a9e6d503e812c48e39c785dc4ef49a4
Grealishgit <eugyneehunter@gmail.com>
2025-08-17 19:04:44 +0300

    Refactor about page styles for improved readability and organization

--- about.php
+++ about.php
@@ -9,9 +9,17 @@
 </head>
 
 <body>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Signika:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+    </style>
+    <style>
+    body {
+        font-family: 'Quicksand', sans-serif;
+    }
+    </style>
     <?php include 'app/Views/navbar.php'; ?>
 
-    <div class="mt-20 flex w-full items-center justify-center min-h-screen">
+    <div class="mt-20 flex w-full  min-h-screen">
         <div class="flex flex-col">
             <h1 class="text-4xl font-bold">About Us</h1>
             <p>Welcome to BlueLife Hospital, where we prioritize your health and well-being.</p>
