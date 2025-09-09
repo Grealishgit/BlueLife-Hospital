8d1caa26133b339f461e77faeb7dd448f1ca7850
Grealishgit <eugyneehunter@gmail.com>
2025-09-09 17:13:35 +0300

    Add image column to users table for profile picture storage

--- app/Queries/create_users_table.php
+++ app/Queries/create_users_table.php
@@ -8,6 +8,7 @@ $sql = "CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    image VARCHAR(255) DEFAULT NULL,
     phone VARCHAR(20) NOT NULL,
     gender VARCHAR(10) NOT NULL,
     dob DATE NOT NULL,
